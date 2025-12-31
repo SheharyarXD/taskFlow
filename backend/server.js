c21a50464548a0fd957fe7ea244550b52d9f0086
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-12-31 21:33:17 +0500

    In p

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,6 @@ async function connectDB() {
   
   if (!cached.promise) {
     const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
     };
