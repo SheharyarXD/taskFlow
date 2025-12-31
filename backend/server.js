553c7b502e10e86bb87582fad6264d4e4e2065b5
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-12-31 20:25:24 +0500

    IN p

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
