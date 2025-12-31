64239931d380ec5fe6a93e9e3a6a9cb58b75ac2c
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-12-31 20:30:56 +0500

    IN p

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,6 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
